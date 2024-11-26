d82eb205233bb34df66742e5effbcd626c1bfecb
Damilola <damilolaoni07@gmail.com>
2024-11-26 19:08:34 +0100

    form api calls

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import axios from 'axios';
+
 
 const Form = () => {
   const [formData, setFormData] = useState({
@@ -7,7 +9,7 @@ const Form = () => {
     email: '',
     countryCode: 'NGN',
     phoneNumber: '',
-    country: '',
+    country: 'Nigeria',
     gender: '',
     experienceLevel: '',
     employmentStatus: '',
@@ -18,18 +20,103 @@ const Form = () => {
     marketingConsent: false,
   });
 
+  const [errors, setErrors] = useState({})
+
+  const validate = () => {
+    const newErrors = {};
+    if (!formData.firstName.trim()) newErrors.firstName = 'First name is required';
+    if (!formData.lastName.trim()) newErrors.lastName = 'Last name is required';
+    if (!formData.email.trim()) newErrors.email = 'Email is required';
+    if (!formData.phoneNumber.trim()) newErrors.phoneNumber = 'Phone number is required';
+    if (!formData.gender) newErrors.gender = 'Gender is required';
+    if (!formData.experienceLevel) newErrors.experienceLevel = 'Experience level is required';
+    if (!formData.employmentStatus) newErrors.employmentStatus = 'Employment status is required';
+    if (!formData.educationLevel) newErrors.educationLevel = 'Education level is required';
+    if (!formData.learningPath) newErrors.learningPath = 'Learning path is required';
+    if (!formData.programStructure) newErrors.programStructure = 'Program structure is required';
+    if (!formData.referralSource) newErrors.referralSource = 'Referral source is required';
+
+    return newErrors;
+  };
+
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
     setFormData({
       ...formData,
       [name]: type === 'checkbox' ? checked : value,
     });
+
+
+    setErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: undefined,
+    }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('Form submitted:', formData);
+    const validationErrors = validate();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
 
+    // console.log('Form submitted:', formData);
+
+    const dataToSend = {
+      first_name: formData.firstName,
+      last_name: formData.lastName,
+      user_email: formData.email,
+      user_phone: formData.phoneNumber,
+      user_country: formData.country,
+      gender: formData.gender,
+      experience_level: formData.experienceLevel,
+      employment_status: formData.employmentStatus,
+      eduacation: formData.educationLevel,
+      learning_path: formData.learningPath,
+      program_structure: formData.programStructure,
+      heard_about_us: formData.referralSource,
+      marketing_consent: formData.marketingConsent,
+    };
+
+    try {
+      console.log ("Submitting data to API...");
+      
+
+      const response = await axios.post("https://apiincubate-production.up.railway.app/api/enroll", dataToSend);
+
+      console.log("API response:", response.data);
+      console.log("Response Status:", response.status);
+
+      setFormData({
+        firstName: '',
+        lastName: '',
+        email: '',
+        countryCode: 'NGN',
+        phoneNumber: '',
+        country: 'Nigeria',
+        gender: '',
+        experienceLevel: '',
+        employmentStatus: '',
+        educationLevel: '',
+        learningPath: '',
+        programStructure: '',
+        referralSource: '',
+        marketingConsent: false,
+      });
+
+      alert ("Form submitted successfully!");
+
+    } catch (error) {
+      if (error.response) {
+        console.error ("Error response: ", error.response.data)
+        alert(`Submission failed: ${error.response.data.message || "An error occurred"}`)
+      } else {
+        console.error("Error:", error.message);
+        alert ("Failed to submit the form. Please try again later.")
+      }
+    }
+    
   };
 
   return (
@@ -50,7 +137,9 @@ const Form = () => {
                 placeholder="First name"
                 className="w-full py-[12px] px-[16px] md:py-[17.56px] md:px-[23.41px] border-[1.46px] border-black rounded-[8.78px] bg-white text-[#434343] font-inter text-[16px] md:text-[23.41px]"
               />
+            {errors.firstName && <p className="text-red-500 text-sm">{errors.firstName}</p>}
             </div>
+
             <div className="flex-1">
               <label htmlFor="lastName" className="block mb-2 text-[14px] md:text-[20.5px] font-inter font-medium text-black">Last name</label>
               <input
@@ -62,7 +151,9 @@ const Form = () => {
                 placeholder="Last name"
                 className="w-full py-[12px] px-[16px] md:py-[17.56px] md:px-[23.41px] border-[1.46px] border-black rounded-[8.78px] bg-white text-[#434343] font-inter text-[16px] md:text-[23.41px]"
               />
+              {errors.lastName && <p className="text-red-500 text-sm">{errors.lastName}</p>}
             </div>
+
           </div>
 
           <div>
@@ -76,6 +167,7 @@ const Form = () => {
               placeholder="Email"
               className="w-full py-[12px] px-[16px] md:py-[17.56px] md:px-[23.41px] border-[1.46px] border-black rounded-[8.78px] bg-white text-[#434343] font-inter text-[16px] md:text-[23.41px]"
             />
+            {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
           </div>
 
           <div>
@@ -100,6 +192,7 @@ const Form = () => {
                 className="flex-1 py-2 ml-4 bg-white w-10 text-[#434343] font-inter text-[16px] md:text-[23.41px] rounded-r-md focus:outline-none"
               />
             </div>
+            {errors.phoneNumber && <p className="text-red-500 text-sm">{errors.phoneNumber}</p>}
           </div>
 
 
@@ -113,7 +206,7 @@ const Form = () => {
                 onChange={handleChange}
                 className="w-full py-[12px] px-[16px] md:py-[17.56px] md:px-[23.41px] border-[1.46px] border-black rounded-[8.78px] bg-white text-[#434343] font-inter text-[16px] md:text-[23.41px]"
               >
-                <option value="">Select Country</option>
+                <option value="Nigeria">Nigeria</option>
               </select>
             </div>
             <div className="flex-1">
@@ -129,6 +222,7 @@ const Form = () => {
                 <option value="male">Male</option>
                 <option value="female">Female</option>
               </select>
+              {errors.gender && <p className="text-red-500 text-sm">{errors.gender}</p>}
             </div>
           </div>
 
@@ -143,7 +237,11 @@ const Form = () => {
                 className="w-full py-[12px] px-[16px] md:py-[17.56px] md:px-[23.41px] border-[1.46px] border-black rounded-[8.78px] bg-white text-[#434343] font-inter text-[16px] md:text-[23.41px]"
               >
                 <option value="">Experience level</option>
+                <option value="Newbie">Newbie</option>
+                <option value="Little experience">Little experience</option>
+                <option value="Experienced">Experienced</option>
               </select>
+              {errors.experienceLevel && <p className="text-red-500 text-sm">{errors.experienceLevel}</p>}
             </div>
             <div className="flex-1">
               <label htmlFor="employmentStatus" className="block mb-2 text-[14px] md:text-[20.5px] font-inter font-medium text-black">Employment Status</label>
@@ -155,7 +253,12 @@ const Form = () => {
                 className="w-full py-[12px] px-[16px] md:py-[17.56px] md:px-[23.41px] border-[1.46px] border-black rounded-[8.78px] bg-white text-[#434343] font-inter text-[16px] md:text-[23.41px]"
               >
                 <option value="">Employment status</option>
+                <option value="Not Employed">Not Employed</option>
+                <option value="Employed">Employed</option>
+                <option value="Self employed">Self employed</option>
+                <option value="Student">Student</option>
               </select>
+              {errors.employmentStatus && <p className="text-red-500 text-sm">{errors.employmentStatus}</p>}
             </div>
           </div>
 
@@ -169,7 +272,14 @@ const Form = () => {
               className="w-full py-[12px] px-[16px] md:py-[17.56px] md:px-[23.41px] border-[1.46px] border-black rounded-[8.78px] bg-white text-[#434343] font-inter text-[16px] md:text-[23.41px]"
             >
               <option value="">Education level</option>
+              <option value="O'Levels">O'Levels</option>
+              <option value="OND">OND</option>
+              <option value="HND">HND</option>
+              <option value="B.Sc">B.Sc</option>
+              <option value="M.Sc">M.Sc</option>
+              <option value="Ph.D">Ph.D</option>
             </select>
+            {errors.educationLevel && <p className="text-red-500 text-sm">{errors.educationLevel}</p>}
           </div>
 
           <div>
@@ -182,7 +292,14 @@ const Form = () => {
               className="w-full py-[12px] px-[16px] md:py-[17.56px] md:px-[23.41px] border-[1.46px] border-black rounded-[8.78px] bg-white text-[#434343] font-inter text-[16px] md:text-[23.41px]"
             >
               <option value="">Select learning path</option>
+              <option value="Tech sales">Tech sales</option>
+              <option value="Product design">Product design</option>
+              <option value="Financial management">Financial management</option>
+              <option value="Product marketing">Product marketing</option>
+              <option value="Customer success">Customer success</option>
+              <option value="Graphic design">Graphic design</option>
             </select>
+            {errors.learningPath && <p className="text-red-500 text-sm">{errors.learningPath}</p>}
           </div>
 
           <div>
@@ -195,7 +312,13 @@ const Form = () => {
               className="w-full py-[12px] px-[16px] md:py-[17.56px] md:px-[23.41px] border-[1.46px] border-black rounded-[8.78px] bg-white text-[#434343] font-inter text-[16px] md:text-[23.41px]"
             >
               <option value="">Preferred program structure</option>
+              <option value="Part-time">Part-time</option>
+              <option value="Full-time">Full-time</option>
+              <option value="Online">Online</option>
+              <option value="Physical">Physical</option>
+              <option value="Hybrid">Hybrid</option>
             </select>
+            {errors.programStructure && <p className="text-red-500 text-sm">{errors.programStructure}</p>}
           </div>
 
           <div>
@@ -208,7 +331,13 @@ const Form = () => {
               className="w-full py-[12px] px-[16px] md:py-[17.56px] md:px-[23.41px] border-[1.46px] border-black rounded-[8.78px] bg-white text-[#434343] font-inter text-[16px] md:text-[23.41px]"
             >
               <option value="">Select option</option>
+              <option value="Through friends">Through friends</option>
+              <option value="Google">Google</option>
+              <option value="Youtube">Youtube</option>
+              <option value="Facebook">Facebook</option>
+              <option value="Twitter">Twitter (X)</option>
             </select>
+            {errors.referralSource && <p className="text-red-500 text-sm">{errors.referralSource}</p>}
           </div>
 
           <div className="flex items-center">
